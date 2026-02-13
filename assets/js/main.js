3173e45bfa5687dbf3664fdef19b2b6715ed1f7d
gerbilbeard <142049863+getbilbeard@users.noreply.github.com>
2026-02-13 08:02:34 -0800

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 console.log('LatchFit site loaded');
+
+const googlePlayLink = document.getElementById('google-play-link');
+
+if (googlePlayLink) {
+  googlePlayLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.alert('Coming soon');
+  });
+}
